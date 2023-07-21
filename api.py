723ed955af1a41df0150b92e36ed1f32ddfd3c28
AZIZBEKRAXMONBERDIYEV <jxechkim079@gmail.com>
2023-07-21 10:53:40 +0500

    randomer

--- api.py
+++ api.py
@@ -1,3 +1,15 @@
 import requests
 
 base_url = "https://randomuser.me/api/"
+
+
+
+
+payload = {
+    'results':100,
+}
+
+response = requests.get(url=base_url,params=payload)
+
+
+
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from tinydb import TinyDB
+from api import response
 
-db = TinyDB('data.json')
+
+db = TinyDB('db.json' , indent=4)
+
+
+users = db.table('users')
+
+
+if response.status_code == 200:
+    users_data = response.json()['results']
+    for user in users_data:
+        users.insert({
+            'first_name':user['name']['first'],
+            'last_name':user['name']['last'],
+            'age':user['dob']['age'],
+            'email':user['email'],
+            'country':user['location']['country'],
+            'phone':user['phone'],
+            
+        })
+
+
